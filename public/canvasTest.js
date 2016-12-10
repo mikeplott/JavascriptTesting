ed4fe33c34f72ddbc41feb13eb67cc86d2857908
michaelplott <mike.plott80@gmail.com>
2016-12-10 12:45:44 -0500

    Canvas drawing correctly

--- public/canvasTest.js
+++ public/canvasTest.js
@@ -17,10 +17,8 @@ function renderImages(data) {
     var xSpeed = 0;
     var ySpeed = 0;
     var startTime = Date.now() + 3000;
-
-    var playerStand = new Image();
-    playerStand.src = sprites[0].fileName;
-    console.log(playerStand);
+    var height = 50;
+    var imageRatio = 1;
 
     var playerStep = new Image();
     playerStep.src = sprites[1].fileName;
@@ -28,10 +26,14 @@ function renderImages(data) {
     var playerLastStep = new Image();
     playerLastStep.src = sprites[2].fileName;
 
-    var animations = [playerStand, playerStep, playerLastStep];
-    console.log(animations[0]);
+    var playerLeftStand = new Image();
+    playerLeftStand.src = sprites[3].fileName;
 
-    var player = playerStand;
+    var playerLeftStep = new Image();
+    playerLeftStep.src = sprites[4].fileName;
+
+    var playerLastLeftStep = new Image();
+    playerLastLeftStep.src = sprites[5].fileName;
 
     var background = new Image();
     background.src = 'ui/test-background.png';
@@ -39,14 +41,63 @@ function renderImages(data) {
         game.drawImage(background, 0, 0, 800, 600);
     }
 
+    var playerStand = new Image();
+    playerStand.src = sprites[0].fileName;
+    playerStand.onload = function() {
+        imageRatio = playerStand.width / playerStand.height;
+    }
+
+    //player = playerStand;
 
     function draw() {
+        game.clearRect(0, 0, theScreen.width, theScreen.height);
         game.drawImage(background, 0, 0, theScreen.width, theScreen.height);
+        game.font = "10px Arial";
+        game.fillStyle = 'white';
+        x = x + xSpeed;
+        y = y + ySpeed;
 
-        function movement(event) {
-            if (event.keycode == 97) {
-                player = 
-            }
+        if (x < 0) {
+            x = 0;
+            player = playerLeftStand;
         }
+        if (x >= 0) {
+            player = playerStand;
+        }
+        if (y >= 0) {
+            y = 0;
+        }
+
+        if (y < 0) {
+            y = 0;
+        }
+
+        if (x > theScreen.width - 30) {
+            x = theScreen.width - 30;
+        }
+
+
+//        document.onkeyDown = movement();
+
+        game.drawImage(player, x, y, height * imageRatio, height);
     }
+    var updateGame = setInterval(draw, 20);
+
+
+    //document.onkeydown = movement();
+    function movement(event) {
+        if (event.keyCode == 37) {
+            xSpeed = -5;
+        }
+        if (event.keyCode == 39) {
+            xSpeed = 5;
+        }
+
+        game.width = game.width;
+        game.drawImage(player, x, y, height * imageRatio, height);
+        game.stroke();
+    }
+
+    document.onkeydown = movement;
+
 }
